2ca046ee9a431c75323e2be14a54e03f26d5f3d2
unknown <damianlukawski11@gmail.com>
2020-11-25 15:19:33 +0100

    started chapter 4

--- chapter3.py
+++ chapter3.py
@@ -1,3 +1,13 @@
 import cv2
 import numpy as np
 
+img = cv2.imread("Resources/lambo.png")
+print(img.shape)
+
+imgResize = cv2.resize(img, (300,200))
+imgCropped = img[0:200,200:500]
+
+cv2.imshow('Image', img)
+cv2.imshow('Image resized', imgResize)
+cv2.imshow('Image cropped', imgCropped)
+cv2.waitKey(0)
